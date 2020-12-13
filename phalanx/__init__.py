a51c70eff041c030ebe48d63f30a574cb968497f
Yunheng Ma <73107124+thewateriswide@users.noreply.github.com>
2020-12-14 02:16:53 +0800

    Updated to a version 1.1.0

--- phalanx/__init__.py
+++ phalanx/__init__.py
@@ -4,7 +4,7 @@ Phalanx, a Python3 package of a simulator for quantum annealer.
 A quantum annealer is a system evolves to a so-called Gibbs state during an 
 annealing process.  The Gibbs state is a superposition of this annealer's
 eigenstate.  Usually, but not always, the Gibbs state is captured as a 
-groundstate,.
+groundstate.
 
 As a computer,  its Hamiltonian can be viewed as a description of a math 
 problem,  and the final measurement is the solution.
